66178e09682bfce3af9a2eb40d2d94696892957e
pdnct4 <Ai TieYuan@pdnct4>
2019-07-12 10:24:53 +0800

    '列表菜单左右联动'

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-      <div class="menu-wrapper">
+      <div class="menu-wrapper" ref="menuWrapper">
         <ul>
-          <li v-for="(item, index) in goods" :key="index" class="menu-item">
+          <li v-for="(item, index) in goods" :key="index" class="menu-item" :class="{'current':currentIndex == index}" @click="selectMenu(index, $event)">
             <span class="text border-1px">
               <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
             </span>
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper">
+      <div class="foods-wrapper" ref="foodsWrapper">
         <ul>
-          <li v-for="(item, index) in goods" :key="index" class="food-list">
+          <li v-for="(item, index) in goods" :key="index" class="food-list food-list-hook">
             <h1 class="title">{{item.name}}</h1>
             <ul>
               <li v-for="(food, index) in item.foods" :key="index" class="food-item">
@@ -22,11 +22,9 @@
                   <h2 class="name">{{food.name}}</h2>
                   <p class="desc">{{food.description}}</p>
                   <div class="extra">
-                    <span class="count">月售{{food.sellCount}}</span>
-                    <span>好评率{{food.rating}}%</span>
+                    <span class="count">月售{{food.sellCount}}</span><span>好评率{{food.rating}}%</span>
                     <div class="price">
-                      <span class="now">￥{{food.price}}</span>
-                      <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                      <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                     </div>
                   </div>
                 </div>
@@ -38,6 +36,7 @@
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 const ERR_OK = 0
 export default {
   name: 'goods',
@@ -48,7 +47,21 @@ export default {
   },
   data () {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0
+    }
+  },
+  computed: {
+    currentIndex () {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i]
+        let height2 = this.listHeight[i + 1]
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i
+        }
+      }
+      return 0
     }
   },
   created () {
@@ -57,15 +70,49 @@ export default {
       response = response.body
       if (response.errno === ERR_OK) {
         this.goods = response.data
-        console.log(this.goods)
+        this.$nextTick(() => {
+          this._initScroll()
+          this._calculateHeight()
+        })
       }
     })
+  },
+  methods: {
+    _initScroll () {
+      this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      })
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType: 3
+      })
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y))
+      })
+    },
+    _calculateHeight () {
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+    },
+    selectMenu (index, event) {
+      if (!event._constructed) {
+        return
+      }
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+      let el = foodList[index]
+      this.foodsScroll.scrollToElement(el, 300)
+    }
   }
 }
 </script>
-<style lang="less" scoped>//stylus less
-@import '../../common/styless/mixin.less';
-@import './goods.less';
-// @import '../../common/styless/mixin.styl';
-// @import './goods.styl';
+<style lang="stylus" scoped>//stylus less
+// @import '../../common/styless/mixin.less';
+// @import './goods.less';
+@import '../../common/styless/mixin.styl';
+@import './goods.styl';
 </style>
